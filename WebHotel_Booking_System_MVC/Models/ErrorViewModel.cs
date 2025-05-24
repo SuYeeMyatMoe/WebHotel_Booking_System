32752d0d09a58dad45a160f09c798b038c64f629
Su_Yee_Myat_Moe <suyeemyatmoe21@gmail.com>
2025-05-24 16:49:00 +0630

    Add project files.

---
+++ WebHotel_Booking_System_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebHotel_Booking_System_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
