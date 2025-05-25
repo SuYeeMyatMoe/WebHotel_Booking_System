040024f74c20255812972da82e998c886f901bbb
Su_Yee_Myat_Moe <suyeemyatmoe21@gmail.com>
2025-05-25 21:37:17 +0630

    Properties for Room Entity

--- Hotel_Booking_System.Domain/Entities/Room.cs
+++ Hotel_Booking_System.Domain/Entities/Room.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace Hotel_Booking_System.Domain.Entities
 {
-    internal class Room
+    public class Room
     {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public double Price { get; set; }
+        public int RoomSize { get; set; }
+        public int Occupancy { get; set; }
+        public string? ImageUrl { get; set; }
+
+        // Tracking in Logging
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+
     }
 }
