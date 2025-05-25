63ba3443e05be36bdd1c541075620d55f3fe114f
Su_Yee_Myat_Moe <suyeemyatmoe21@gmail.com>
2025-05-25 22:00:31 +0630

    Add Contructure and Dependency injection to pass any parameters in DBContext to base class

--- Hotel_Booking_System.Infrastructure/Data/ApplicationDbContext.cs
+++ Hotel_Booking_System.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,10 @@ using System.Threading.Tasks;
 
 namespace Hotel_Booking_System.Infrastructure.Data
 {
-    internal class ApplicationDbContext
+    public class ApplicationDbContext:DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options) 
+        {
+        }
     }
 }
