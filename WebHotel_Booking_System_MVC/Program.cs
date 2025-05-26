e7c46ecfc6f287fb0f8eb3f90dda2d106b86cb88
Su_Yee_Myat_Moe <suyeemyatmoe21@gmail.com>
2025-05-26 21:59:00 +0630

    Configure DbContext to use SQL server in MVC pj

--- WebHotel_Booking_System_MVC/Program.cs
+++ WebHotel_Booking_System_MVC/Program.cs
@@ -1,7 +1,14 @@
+using Hotel_Booking_System.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(opition=>opition.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+//we can use GetConnectionString function (default one due to using ConnectionStrings in appSetting if not must use GetValue function)
+);//options are already defined in ApplicationDbContext as DbContext Options
 
 var app = builder.Build();
 
