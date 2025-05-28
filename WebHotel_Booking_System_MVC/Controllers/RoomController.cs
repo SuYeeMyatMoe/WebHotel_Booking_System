2cd55deceb72b05e5887b8f13217cb45f5ae5c62
Su_Yee_Myat_Moe <suyeemyatmoe21@gmail.com>
2025-05-28 22:21:54 +0630

    Adding RoomController

---
+++ WebHotel_Booking_System_MVC/Controllers/RoomController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebHotel_Booking_System_MVC.Controllers
+{
+    public class RoomController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
