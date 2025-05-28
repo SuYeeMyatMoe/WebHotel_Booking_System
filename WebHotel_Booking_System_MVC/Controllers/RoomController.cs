5cc8858b397d6eeaf0b7fe86d1c88f0e9d1bd27f
Su_Yee_Myat_Moe <suyeemyatmoe21@gmail.com>
2025-05-28 22:39:31 +0630

    Dependency injection for DbContext

--- WebHotel_Booking_System_MVC/Controllers/RoomController.cs
+++ WebHotel_Booking_System_MVC/Controllers/RoomController.cs
@@ -1,11 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Hotel_Booking_System.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebHotel_Booking_System_MVC.Controllers
 {
+    
+    //Instead of registering DbContext directly, as DbContext is registered in program.cs, we can inject dbcontext and automatically configure connectionstring, open connection and give implemation for constructor
+    
     public class RoomController : Controller
     {
+        private readonly ApplicationDbContext _dbContext;
+
+        public RoomController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         public IActionResult Index()
         {
+            var rooms=_dbContext.Rooms.ToList();
             return View();
         }
     }
