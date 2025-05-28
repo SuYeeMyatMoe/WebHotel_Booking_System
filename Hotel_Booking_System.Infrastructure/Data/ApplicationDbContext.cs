8580f1c2ca2f11983ea780f1e60e04da8341607d
Su_Yee_Myat_Moe <suyeemyatmoe21@gmail.com>
2025-05-28 15:51:31 +0630

    Using modelBuilder (Created 3 Room Type to add data from Room entity into Rooms Table in dbs)

--- Hotel_Booking_System.Infrastructure/Data/ApplicationDbContext.cs
+++ Hotel_Booking_System.Infrastructure/Data/ApplicationDbContext.cs
@@ -18,7 +18,42 @@ namespace Hotel_Booking_System.Infrastructure.Data
         //Adding Initial record for Room in DBS
         protected override void OnModelCreating(ModelBuilder modelBuilder)//protected override and choose OnModel in dropdown
         {
-            base.OnModelCreating(modelBuilder);
+            //base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Room>()
+                .HasData(
+                new Room
+                {
+                    Id = 1,
+                    Name = "Room 1",
+                    Description = "Classic Single Room",
+                    PriceinDoller = 30,
+                    RoomSize = 260,//area length in square ft (20ft*13ft)
+                    Occupancy = 1,
+                    ImageUrl = "https://www.classichotel.al/wp-content/uploads/2019/07/Optimized-single1.jpg"
+
+                },
+                new Room
+                {
+                    Id = 2,
+                    Name = "Room 2",
+                    Description = "Deluxe Double Room",
+                    PriceinDoller = 50,
+                    RoomSize = 400, // 20ft * 20ft
+                    Occupancy = 2,
+                    ImageUrl = "https://www.pearlhotelnyc.com/hs-fs/hubfs/2.jpg?width=1488&height=992&name=2.jpg"
+                },
+
+                new Room
+                {
+                    Id = 3,
+                    Name = "Room 3",
+                    Description = "Executive Suite",
+                    PriceinDoller = 90,
+                    RoomSize = 600, // 30ft * 20ft
+                    Occupancy = 3,
+                    ImageUrl = "https://image-tc.galaxy.tf/wijpeg-eyy6alr2se13uffg555o3iapg/executive-suite-room-2_wide.jpg"
+                });
+
         }
     }
 }
