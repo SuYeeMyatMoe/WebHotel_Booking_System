b009de8fada4f54556b352096b9d1273b61f4e6d
Su_Yee_Myat_Moe <suyeemyatmoe21@gmail.com>
2025-05-28 15:35:45 +0630

    update one property's name in Room entity and add-migration and update-database to also update in script and dbs table name

--- Hotel_Booking_System.Domain/Entities/Room.cs
+++ Hotel_Booking_System.Domain/Entities/Room.cs
@@ -11,7 +11,7 @@ namespace Hotel_Booking_System.Domain.Entities
         public int Id { get; set; }
         public required string Name { get; set; }
         public string? Description { get; set; }
-        public double Price { get; set; }
+        public double PriceinDoller { get; set; }
         public int RoomSize { get; set; }
         public int Occupancy { get; set; }
         public string? ImageUrl { get; set; }
---
+++ Hotel_Booking_System.Infrastructure/Migrations/20250528083430_UpdatingPriceName.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hotel_Booking_System.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatingPriceName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Price",
+                table: "Rooms",
+                newName: "PriceinDoller");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PriceinDoller",
+                table: "Rooms",
+                newName: "Price");
+        }
+    }
+}
