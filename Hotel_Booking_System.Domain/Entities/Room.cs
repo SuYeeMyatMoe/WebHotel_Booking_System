941f01c27dd3df57d3b96476e7640fd9e51b844c
Su_Yee_Myat_Moe <suyeemyatmoe21@gmail.com>
2025-05-30 16:33:51 +0630

    data annotation in .NET. to displaying custom labels instead of property name in user display

--- Hotel_Booking_System.Domain/Entities/Room.cs
+++ Hotel_Booking_System.Domain/Entities/Room.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,8 @@ namespace Hotel_Booking_System.Domain.Entities
         public int Id { get; set; }
         public required string Name { get; set; }
         public string? Description { get; set; }
+
+        [Display(Name = "Price ($)")]
         public double PriceinDoller { get; set; }
         public int RoomSize { get; set; }
         public int Occupancy { get; set; }
