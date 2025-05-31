81c693c91438bc0cffd667108265f25aa7032e3c
Su_Yee_Myat_Moe <suyeemyatmoe21@gmail.com>
2025-05-31 09:49:05 +0630

    Basic implementation of CreateRoom Post endpoint

--- WebHotel_Booking_System_MVC/Controllers/RoomController.cs
+++ WebHotel_Booking_System_MVC/Controllers/RoomController.cs
@@ -1,4 +1,5 @@
-﻿using Hotel_Booking_System.Infrastructure.Data;
+﻿using Hotel_Booking_System.Domain.Entities;
+using Hotel_Booking_System.Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -21,9 +22,22 @@ namespace WebHotel_Booking_System_MVC.Controllers
             var rooms=await _dbContext.Rooms.ToListAsync();
             return View(rooms);//display the list in View by passing rooms parameter
         }
+
+        //Get endpoint when user open the form
+        [HttpGet]
         public async Task<IActionResult> CreateRoom()
         {
-            return View();
+            return View();//it means no data is reloaded in form when it is opened.
+        }
+
+        //Post endpoint when user submitted the data for CreateRoom form and .net core will automatically provide Model in view when the form is submitted. 
+        //Because of that, we can retrieve data and extract value from model
+        [HttpPost]
+        public async Task<IActionResult> CreateRoom(Room room)
+        {
+            await _dbContext.Rooms.AddAsync(room);
+            await _dbContext.SaveChangesAsync();
+            return RedirectToAction("Index", "Room");
         }
     }
 }
