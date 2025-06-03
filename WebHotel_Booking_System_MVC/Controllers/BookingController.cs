aec0a601d809b1715b5947a60769300cb4cce777
Su_Yee_Myat_Moe <suyeemyatmoe21@gmail.com>
2025-06-03 22:17:19 +0630

    Add Booking Controller

---
+++ WebHotel_Booking_System_MVC/Controllers/BookingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebHotel_Booking_System_MVC.Controllers
+{
+    public class BookingController : Controller
+    {
+        public IActionResult BookNow()
+        {
+            return View();
+        }
+    }
+}
