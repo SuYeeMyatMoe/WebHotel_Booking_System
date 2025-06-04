376514529234caed3a2d26eafe7799aed1febd6d
Su_Yee_Myat_Moe <suyeemyatmoe21@gmail.com>
2025-06-04 20:39:45 +0630

    Add ContactUs_Controller with Contact action method

---
+++ WebHotel_Booking_System_MVC/Controllers/ContactUs_Controller.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebHotel_Booking_System_MVC.Controllers
+{
+    public class ContactUs_Controller : Controller
+    {
+        [HttpGet]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
