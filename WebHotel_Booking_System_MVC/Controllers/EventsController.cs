d863d6aee9e5ebc4fffb57f7f15612569a9b82c9
Su_Yee_Myat_Moe <suyeemyatmoe21@gmail.com>
2025-06-05 22:33:36 +0630

    Add EventsController

---
+++ WebHotel_Booking_System_MVC/Controllers/EventsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebHotel_Booking_System_MVC.Controllers
+{
+    public class EventsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
