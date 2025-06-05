e26239d482011b681d0394f03619c2a305690268
Su_Yee_Myat_Moe <suyeemyatmoe21@gmail.com>
2025-06-05 22:44:28 +0630

    Use fixed data currently for events in EventsController

--- WebHotel_Booking_System_MVC/Controllers/EventsController.cs
+++ WebHotel_Booking_System_MVC/Controllers/EventsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Hotel_Booking_System.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace WebHotel_Booking_System_MVC.Controllers
 {
@@ -6,7 +8,15 @@ namespace WebHotel_Booking_System_MVC.Controllers
     {
         public IActionResult EventList()
         {
-            return View();
+            // It is Sample data — later, I will pull this from a database
+            var events = new List<Event>
+            {
+                new Event { Title = "Summer Festival", Description = "Join us for a fun-filled summer!", ImageUrl = "/images/summer-festival.jpg" },
+                new Event { Title = "Wine Tasting Night", Description = "Experience the best wines.", ImageUrl = "/images/wine-night.jpg" },
+                new Event { Title = "New Year's Eve Gala", Description = "Celebrate the new year in style.", ImageUrl = "/images/newyear-gala.jpg" }
+            };
+
+            return View(events);
         }
     }
 }
