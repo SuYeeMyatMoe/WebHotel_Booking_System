0bf034fe517308049ca9422c6ee8948e731fb028
Su_Yee_Myat_Moe <suyeemyatmoe21@gmail.com>
2025-06-07 21:38:44 +0630

    range attribuate with error message for error handling

--- Hotel_Booking_System.Domain/Entities/Room.cs
+++ Hotel_Booking_System.Domain/Entities/Room.cs
@@ -14,10 +14,13 @@ namespace Hotel_Booking_System.Domain.Entities
         public string? Description { get; set; }
 
         [Display(Name = "Price ($)")]
+        [Range(10,10000, ErrorMessage = "Price per night must be at least $10 and at most $10,000.")]//at least $10 and at most $10,000.
         public double PriceinDoller { get; set; }
 
         [Display(Name = "Room Size (Sqft)")]
         public int RoomSize { get; set; }
+
+        [Range(1, 10, ErrorMessage = "Occupancy must be between 1 and 10.")]//between 1 and 10 to prevent 0 and overtyping
         public int Occupancy { get; set; }
 
         [Display(Name = "Image URL)")]
