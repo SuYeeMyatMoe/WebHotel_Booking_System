6624561d8fa87c110832c8e4bf75712291908929
Su_Yee_Myat_Moe <suyeemyatmoe21@gmail.com>
2025-06-12 21:13:43 +0630

    Model class for roomdetails class

--- Hotel_Booking_System.Domain/Entities/RoomDetails.cs
+++ Hotel_Booking_System.Domain/Entities/RoomDetails.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Hotel_Booking_System.Domain.Entities
 {
-    internal class RoomDetails
+    public class RoomDetails
     {
+        [Key]
+        public int Id { get; set; }
+
+        // Foreign Key for Room
+        [Required]
+        public int RoomId { get; set; }
+
+        [ForeignKey("RoomId")]
+        public virtual Room Room { get; set; }
+
+        // Foreign Key for Booking
+        [Required]
+        public int BookingId { get; set; }
+
+        [ForeignKey("BookingId")]
+        public virtual Booking Booking { get; set; }
+
+        // Additional properties (optional)
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+        public decimal PricePerNight { get; set; }
+        public int NumberOfGuests { get; set; }
     }
 }
